39ec46721a6e7977e2f99205c9e39cdb8657ba86
Eloy Rengel <eloypanita@gmail.com>
2025-05-26 00:16:03 -0400

    condicionando el main

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -3,10 +3,12 @@ import '../App.css'
 function Footer(){
     return(
         <>
+        
         <div className="footer">
         <h1>Eloy Rengel</h1>
 
         </div>
+        
         </>
     )
 }
